daa8de8c6a15c03c3eaf8a5c84a774dbdba10fe7
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-12-02 21:14:20 -0500

    passes all tests!

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 20, "M", "Brown", "Angry", 0),
+("Rowdy", 10, "M", "Brown", "Angry", 1),
+("Tabitha", 6, "F", "Brown", "Angry", 1),
+("Sergeant Brown", 19, "M", "Brown", "Angry", 0),
+("Melissa", 13, "F", "Brown", "Angry", 1),
+("Grinch", 2, "M", "Brown", "Angry", 1),
+("Wendy", 6, "F", "Brown", "Angry", 1),
+(NULL, 4, "M", "Brown", "Angry", 0);
